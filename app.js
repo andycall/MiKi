baac0bca89757934a8596a3bb6b5bb2e3a4944da
ErosZy <395693101@qq.com>
2014-08-27 22:53:05 +0800

    init

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require(DIR_PATH+'express');
 var ejs = require(DIR_PATH+"/ejs");
 var app = express();
 
+console.log(ejs.renderFile("./index.html",{
+    title:"hahaha"
+}));
+
 app.get('/hello', function(req, res){
     var body = 'Hello World';
     res.setHeader('Content-Type', 'text/plain');
